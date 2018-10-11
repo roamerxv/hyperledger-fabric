e6a0d90268865f27696b4df3b06b45feba6d4181
徐泽宇 <xu.zeyu@z2a.com.cn>
2018-10-11 16:17:28 +0800

    remove fabric 1.0.0 update ansible yaml

--- docker-compose-files/hyperledger/1.0.0/docker-clean.sh
+++
@@ -1,9 +0,0 @@
-echo "删除所有容器"
-docker rm -f $(docker ps -aq)
-
-echo "删除相关镜像"
-docker rmi -f $(docker images 08_* -aq)
-docker rmi -f $(docker images dev-* -aq)
-
-echo "启用 roamerxv 的 fabric-javaenv 镜像"
-docker tag  roamerxv/fabric-javaenv hyperledger/fabric-javaenv:x86_64-1.0.0-preview
